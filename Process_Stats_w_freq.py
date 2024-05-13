a5f446560bb844909b4cfaeb3088b70c1c1e1270
Mr. Reinoso <ramiro.reinoso@verizon.net>
2024-05-13 19:14:30 -0400

    FIrst batch test of frequency sweep.

---
+++ Process_Stats_w_freq.py
@@ -0,0 +1,97 @@
+import pandas as pd
+
+altitudes = [50]
+frequencies = [4030,4040,4050,4060,4070,4080,4090,4100,4110,4120,4130]
+filefolder="ALT-55B-May13-24-01"
+radar="ALT-55B"
+genminpower = -25
+genmaxpower = -10
+minpowerforplot = genminpower - 10
+genpwrtopsd=17.8 # Add this to 5G gen power to get PSD
+
+
+
+# Calculate the statistics
+
+# Start frequency loop
+for j in frequencies:
+    
+    for x in altitudes:
+        infilename=filefolder+"\\"+radar+"_"+str(j)+"_"+str(x)+".csv"
+        print(infilename)
+        outfilename=filefolder+"\\"+radar+"_"+str(j)+"_"+str(x)+"_stats.csv"
+
+        simul=pd.read_csv(infilename)
+
+        # Compute the baseline statistics (the first 60 seconds with power OFF)
+        base=simul[(simul["rfon"] == 0) & (simul["time"] < 65)]
+        basemean=base["alt"].mean()
+        base1stptile=base["alt"].quantile(0.01)
+        base99thptile=base["alt"].quantile(0.99)
+        basemin=base["alt"].min()
+        basemax=base["alt"].max()
+        basestd=base["alt"].std()
+
+        outdf=pd.DataFrame({'pwr':["Baseline"],'psd':["Baseline"],'mean':[basemean],'1stptile':[base1stptile],'99thptile':[base99thptile],
+                            'meantest':["N/A"],'1stptiletest':["N/A"],'99ptiletest':["N/A"],'min':[basemin],'max':[basemax],
+                            'std':[basestd],'meanerror':["N/A"],'minerror':["N/A"],
+                            'maxerror':["N/A"],'ptile1sterror':["N/A"],'ptile99therror':["N/A"],'stderror':["N/A"]})
+
+
+        for i in range(genminpower,genmaxpower):
+            tmppwr=simul[simul["pwr"] == i]
+
+            thismean=tmppwr["alt"].mean()
+            this1stptile=tmppwr["alt"].quantile(0.01)
+            this99thptile=tmppwr["alt"].quantile(0.99)
+            thismin=tmppwr['alt'].min()
+            thismax=tmppwr['alt'].max()
+            thisstd=tmppwr['alt'].std()   
+
+            # Calculate test inputs
+            meantest = abs(basemean - thismean)/thismean * 100
+            ptile1sttest = basemean - (0.02 * basemean)
+            ptile99thtest = basemean + (0.02 * basemean)
+
+            # print("Power: ",str(i))
+            # print(thismean)
+            # print(this1stptile)
+            # print(this99thptile)
+
+            if meantest > 0.5:
+                mtest="FAIL"
+            else:
+                mtest="PASS"
+
+            if this1stptile < ptile1sttest:
+                priptile1sttest="FAIL"
+            else:
+                priptile1sttest="PASS"
+
+            if this99thptile > ptile99thtest:
+                priptile99thtest="FAIL"
+            else:
+                priptile99thtest="PASS"
+
+            # Error with respect to the baseline data
+            meanerror = (basemean - thismean)/basemean * 100
+            minerror = (basemin - thismin)/basemin * 100
+            maxerror = (basemax - thismax)/basemax * 100
+            ptile1sterror = (base1stptile - this1stptile)/base1stptile * 100
+            ptile99therror = (base99thptile - this99thptile)/base99thptile * 100
+            stderror = (basestd - thisstd)/basestd * 100
+
+            tmprow={'pwr': i,'psd':round(i+genpwrtopsd,1),'mean': thismean,'1stptile': this1stptile,'99thptile': this99thptile,
+                            'meantest': mtest,'1stptiletest': priptile1sttest,'99ptiletest': priptile99thtest,
+                            'min':thismin,'max':thismax,'std':thisstd,'meanerror':meanerror,'minerror':minerror,
+                            'maxerror':maxerror,'ptile1sterror':ptile1sterror,'ptile99therror':ptile99therror,
+                            'stderror':stderror}
+            
+            outdf = outdf._append(tmprow, ignore_index=True)
+
+        outdf.to_csv(outfilename)
+
+exit()
+
+
+
---
+++ Summary_of_Limits_w_freq.py
@@ -0,0 +1,97 @@
+import pandas as pd
+
+altitudes = [50]
+frequencies = [4030,4040,4050,4060,4070,4080,4090,4100,4110,4120,4130]
+
+filefolder="ALT-55B-May13-24-01"
+radar="ALT-55B"
+
+genpwrtopsd=17.8 # Add this to 5G gen power to get PSD
+
+# Crate the output filename
+outfilename=filefolder+"\\"+radar+"_summary.csv"
+
+# Create the output Pandas DataFrame
+outdf=pd.DataFrame({'freq':[],'alt':[],'fail':[],'mean':[],'1stptile':[],'99thptile':[],
+                        'lastgood':[],'limit':[]})
+
+# Calculate the statistics
+
+# Loop through all frequencies
+
+for j in frequencies:
+
+    for x in altitudes:
+        # Open the statistics file for this altitude and create a Pandas DataFrame
+        infilename=filefolder+"\\"+radar+"_"+str(j)+"_"+str(x)+"_stats.csv"
+        stats=pd.read_csv(infilename)
+
+        # Drop the first row as it contains the baseline statistics which we don't need
+        stats = stats.drop([0])
+
+        # Convert the power and psd columnt to integer and float respectively
+        stats["pwr"] = pd.to_numeric(stats["pwr"],downcast="integer")
+        stats["psd"] = pd.to_numeric(stats["psd"])
+
+        # Set the range for min power and max power for the for loop
+        genminpower = stats["pwr"].min()
+        genmaxpower = stats["pwr"].max()
+        genmaxpower = genmaxpower + 1
+
+        mflag = False
+        p1flag = False
+        p99flag = False
+        m = ""
+        p1 = ""
+        p99 = ""
+        psd = 0.0
+
+        for i in range(genminpower,genmaxpower):
+        
+            # Filter the dataframe to find the row that corresponds to the power level
+            # of interest.
+            tmppwr=stats[stats["pwr"] == i]
+
+            # Calculate the index that correspond to the row of the selected power level.
+            # Even thouth the tmppwr vector has only one row, the index of that row is not
+            # zero but whatever the index was on the stats dataframe.
+            index = abs(genminpower) + i + 1
+
+            meantest = tmppwr.loc[index,"meantest"]
+            p1test = tmppwr.loc[index,"1stptiletest"]
+            p99test = tmppwr.loc[index,"99ptiletest"]
+            psd = tmppwr.loc[index,"psd"]
+
+            if meantest == "FAIL":
+                mflag = True
+                m = "*"
+
+            if p1test == "FAIL":
+                p1flag = True
+                p1 = "*"
+
+            if p99test == "FAIL":
+                p99flag = True
+                p99 = "*"
+
+            if mflag or p1flag or p99flag:
+                break
+
+        if not mflag and not p1flag and not p99flag:
+            fail = "NF"
+            good = psd
+        else:
+            fail = psd
+            good = psd - 1
+        
+        tmprow={'freq':j,'alt': x,'fail': fail,'mean': m,'1stptile': p1,'99thptile': p99,
+                            'lastgood': good,'limit': "N/A"}
+            
+        outdf = outdf._append(tmprow, ignore_index=True)
+
+outdf.to_csv(outfilename)
+
+exit()
+
+
+
